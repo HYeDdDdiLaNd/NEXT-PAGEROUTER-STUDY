aca240fc839f30840e7b9c869fff69f4a8354c33
HYeDdDiLaNd <anotherHyedddi@gmail.com>
2025-07-20 15:41:09 +0900

    next router first study

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,50 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const onClickBtnByMoveTestPage = () => {
+    router.push('/test');
+  };
+
+  const onClickBtnByMoveBack = () => {
+    router.back();
+  };
+
+  const onClickBtnDontBack1 = () => {
+    /* 쿼리스트링으로 이동 */
+    router.replace({
+      pathname: '/search',
+      query: { q: 'apple' },
+    });
+  };
+
+  const onClickBtnDontBack2 = () => {
+    /* url 파라미터로 이동 */
+    router.replace('/book/[[...id]]', '/book/123');
+  };
+
+  return (
+    <>
+      <nav>
+        <Link href={'/'}>index</Link>&nbsp;
+        {/* next에서 제공되는 Link를 사용한다. 렌더링 방식의 링크 생성 */}
+        <Link href={'/search'}>search</Link>&nbsp;
+        <Link href={'/book/1'}>book/1</Link>&nbsp;
+        {/* 프로그래메틱 하게 페이지 이동: 사용자가 링크를 클릭해서 이동이 아닌 이벤트에 의해 함수가 실행되어 페이지가 이동되는 것을 말함. */}
+        <button onClick={onClickBtnByMoveTestPage}>테스트 페이지로 이동</button>
+        <button onClick={onClickBtnByMoveBack}>뒤로가기</button>
+        <button onClick={onClickBtnDontBack1}>
+          뒤로가기 방지: 쿼리 스트링
+        </button>
+        <button onClick={onClickBtnDontBack2}>
+          뒤로가기 방지: url 파라미터
+        </button>
+      </nav>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router';
+
+export default function Page() {
+  const router = useRouter();
+  console.log(router);
+  const { q } = router.query; //쿼리 스트링: 페이지 경로 뒤에 물음표가 붙으면 search?q=정혜진
+
+  return (
+    <>
+      <h2>{q}search 룰루리</h2>
+    </>
+  );
+}
